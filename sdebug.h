a44f3755640c94416ffecb7f1ef81c53969bb68c
Sorush <sorush.kh@gmail.com>
2018-10-24 23:52:34 +0100

    sdebug added

---
+++ sdebug.h
@@ -0,0 +1,27 @@
+#ifdef SDEBUG
+
+  // NDEBUG switches assertation off. For safety, it is undefined.
+  #undef NDEBUG
+  #include <cassert>
+
+  // Commands which are run only in debug mode.
+  #define D(x) x
+
+  // SASSERT function 
+  #define SASSERT(condition, message)                                             \
+  {                                                                              \
+      if(!(condition))                                                           \
+      {                                                                          \
+          std::cerr << message<<endl;                                            \
+          std::cerr << "Assertion failed at " << __FILE__ << ":" << __LINE__;    \
+          std::cerr << " inside " << __FUNCTION__ << std::endl;                  \
+          std::cerr << "Condition: " << #condition;                              \
+          abort();                                                               \
+      }                                                                          \
+  }
+#else
+
+  // Do none of above in release mode.
+  #define D(x) 
+  #define SASSERT(condition, message) 
+#endif
