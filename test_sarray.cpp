c0c215df63ed8458136f84ae770a085c8106199b
sorush <sorush.kh@gmail.com>
2018-12-02 21:03:27 +0000

    generalize a test

--- test_sarray.cpp
+++ test_sarray.cpp
@@ -37,6 +37,7 @@ int main() {
 	return 0;
 #endif
 
+	// Print results on screen.
 	bool isPrint = false;
 
 
@@ -198,7 +199,8 @@ int main() {
 	SASSERT(k.getSize() == 2, "Error");
 	cout << "   OK. \n\n";
 
-	cout << "===========Sarray & Sarray==============" << endl;
+	cout << "=========== Sarray-Sarray Operations ==============" << endl;
+	if (isPrint) cout << "Array c = {2.1,2.1}, h={2.,2.} is used below." << endl;
 	cout << " + operator ... " << endl;
 	k = c + h;
 	if (isPrint) cout << "k=c+h " << endl;
@@ -234,9 +236,12 @@ int main() {
 	cout << "   OK. \n\n";
 
 
-	cout << "===========Sarray & Scalar & Sarray==============" << endl;
+	cout << "=========== Sarray-Scalar-Sarray (double-int) Mix Operation ==============" << endl;
+	if (isPrint) cout << "Sarray<double> c = {2.1,2.1}, h={2.0,2.0}, and Sarray<int> i={1,1} are used below." << endl;
+	sarint i = { 1,1 };
+
 	cout << "Mix operator ..." << endl;
-	k = c / 2.0 * h + 1.0 - 2 * c*0.5 - (+h)*(-c) + c / (0.5*h);
+	k = i+c / 2.0 * h*i + 1.0/i - 2 *i* c*0.5 - (+h)*(-c) + c / (0.5*h)-i;
 	if (isPrint) cout << "k = c/0.5 * h + 1.0-2*c*0.5-(+h)*(-c) = {7.3,7.3} " << endl;
 	if (isPrint) cout << k;
 	SASSERT(eq(k[0], 7.3) && eq(k[1], 7.3), "Error");
@@ -244,9 +249,8 @@ int main() {
 
 
 	cout << "===========Integer Sarray==============" << endl;
-	sarint i = { 1,1 };
-	sarint j = { 10,10 };
 	sarint l(0, 2);
+	sarint j = { 10,10 };
 	l = 2 * j / 2 - j / (-i) + 3 * j*i + i / 100;
 	if (isPrint) cout << "sarint i={1,1},  sarint j={10,10}, sarint l(0,2)" << endl;
 	if (isPrint) cout << "l = 2*j/2 - j/(-i) + 3*j*i + i/100" << endl;
@@ -256,25 +260,24 @@ int main() {
 
 
 	cout << "====== Implitcit type casting: Integer * Double =========" << endl;
-	if (!isPrint) cout << "\nSwitch on isPrint variable to see the results of this test.\n" << endl;
 	if (isPrint) cout << "Sarray handles automatic type promotion of arithmetic operations:" << endl;
 	if (isPrint) cout << "c={2.1,2.1}, i={1,1}" << endl;
 	if (isPrint) cout << "-----> +operator \n\n";
 	if (isPrint) cout << "Type of i+c is " << typeid(i + c).name() << " result =  " << i + c;
 	if (isPrint) cout << "Type of c+i is " << typeid(c + i).name() << " result =  " << c + i;
-	if (isPrint) cout << "Type of i+1.5 is "<<typeid(i+1.5).name() <<" result = "<< i + 1.5;
-	if (isPrint) cout << "Type of 1.5+i is "<<typeid(1.5+i).name() <<" result = "<< 1.5 + i;
-	SASSERT(typeid(i+c)==typeid(sardouble)&& typeid(c+i) == typeid(sardouble) && 
-		    typeid(i + 1.5) == typeid(sardouble) && typeid(1.5+i) == typeid(sardouble),"Error!" );
+	if (isPrint) cout << "Type of i+1.5 is " << typeid(i + 1.5).name() << " result = " << i + 1.5;
+	if (isPrint) cout << "Type of 1.5+i is " << typeid(1.5 + i).name() << " result = " << 1.5 + i;
+	SASSERT(typeid(i + c) == typeid(sardouble) && typeid(c + i) == typeid(sardouble) &&
+		typeid(i + 1.5) == typeid(sardouble) && typeid(1.5 + i) == typeid(sardouble), "Error!");
 	if (isPrint) cout << "-----> -operator \n\n";
-    if (isPrint) cout << "Type of i-c is " << typeid(i - c).name() << " result =  " << i - c;
+	if (isPrint) cout << "Type of i-c is " << typeid(i - c).name() << " result =  " << i - c;
 	if (isPrint) cout << "Type of c-i is " << typeid(c - i).name() << " result =  " << c - i;
 	if (isPrint) cout << "Type of i-1.5 is " << typeid(i - 1.5).name() << " result = " << i - 1.5;
 	if (isPrint) cout << "Type of 1.5-i is " << typeid(1.5 - i).name() << " result = " << 1.5 - i;
 	SASSERT(typeid(i - c) == typeid(sardouble) && typeid(c - i) == typeid(sardouble) &&
 		typeid(i - 1.5) == typeid(sardouble) && typeid(1.5 - i) == typeid(sardouble), "Error!");
 	if (isPrint) cout << "-----> *operator \n\n";
-    if (isPrint) cout << "Type of i*c is " << typeid(i * c).name() << " result =  " << i * c;
+	if (isPrint) cout << "Type of i*c is " << typeid(i * c).name() << " result =  " << i * c;
 	if (isPrint) cout << "Type of c*i is " << typeid(c * i).name() << " result =  " << c * i;
 	if (isPrint) cout << "Type of i*1.5 is " << typeid(i * 1.5).name() << " result = " << i * 1.5;
 	if (isPrint) cout << "Type of 1.5*i is " << typeid(1.5 * i).name() << " result = " << 1.5 * i;
@@ -291,7 +294,6 @@ int main() {
 	cout << "   OK. \n\n";
 
 	cout << "====== Explicit type casting =========" << endl;
-	if (!isPrint) cout << "\nSwitch on isPrint variable to see the results of this test.\n" << endl;
 	if (isPrint)cout << "Sarray can be cast to desired type, if similar casting is available for its elements. \n"
 		"For example, int -> double is defined by C++ compilers; therefore it is available by  \n"
 		"Sarray too.\n";
@@ -300,6 +302,9 @@ int main() {
 	if (isPrint)cout << "Sarray<double>(i) * c = " << sardouble(i) * c;
 	if (isPrint)cout << "(Sarray<int>)c * i = " << (sarint)c * i;
 	if (isPrint)cout << "Sarray<int>(c) * i = " << sarint(c) * i;
+	SASSERT(typeid((sardouble)i * c) == typeid(sardouble) && typeid(sardouble(i) * c) == typeid(sardouble) &&
+		typeid((sarint)c * i) == typeid(sarint) && typeid(sarint(c) * i) == typeid(sarint), "Error!");
+	cout << "   OK. \n\n";
 
 	cout << "===========Functions==============" << endl;
 
